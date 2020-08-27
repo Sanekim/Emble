d768130f42f563097ab352699db6a26f4f6b6c38
PH5555 <47862506+PH5555@users.noreply.github.com>
2020-08-28 08:35:40 +0900

    산이나쁘다
    ㅎㅎ

---
+++ Assets/02.Script/changeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //미션 클리어
+        if (Input.GetKey(KeyCode.Escape)) {
+            gameObject.GetComponent<MeshRenderer>().material = Resources.Load("light_green") as Material;
+        }
+    }
+}
